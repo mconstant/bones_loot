6413bf22b0fb8e1f407484cab7b98b8a27dd8fd5
mconstant <bobbaxter@protonmail.com>
2023-06-18 21:28:16 -0400

    zip together arrays

--- gacha.js
+++ gacha.js
@@ -6,15 +6,24 @@ import fs from 'fs';
 import { RandomPicker } from "./randomPicker.js";
 
 const familiars_yaml = 'lootverse/familiars.yaml';
+const familiar_threads_yaml = 'lootverse/familiar_threads.yaml';
 
 const familiars = yaml.load(fs.readFileSync(familiars_yaml, {encoding: 'utf-8'}));
+const familiar_threads = yaml.load(fs.readFileSync(familiar_threads_yaml, {encoding: 'utf-8'}));
 
-let picker;
-picker = new RandomPicker(familiars);
+let familiar_picker = new RandomPicker(familiars);
+let familiar_threads_picker = new RandomPicker(familiar_threads);
 
 // console.log(familiars)
 
-console.log(picker.pickManyDistinct(16))
+const distinct_familiars = familiar_picker.pickManyDistinct(16)
+const distinct_familiar_threads = familiar_threads_picker.pickManyDistinct(16)
+
+const zipped = distinct_familiars.map((e, i) => {
+    return ['familiar: ' + e, 'familiar_threads: ' + distinct_familiar_threads[i]]
+})
+
+console.log(zipped)
 
 
 
